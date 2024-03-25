89990a82a79fe98b94e4de002cef0791226a5826
CodifyArhab <fdzakyarhab@gmail.com>
2024-03-25 09:29:19 +0700

    first commit

---
+++ tugasnetbean/TugasNetbean.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tugasnetbean;
+
+/**
+ *
+ * @author user
+ */
+public class TugasNetbean {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
